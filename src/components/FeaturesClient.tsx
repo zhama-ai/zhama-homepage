0e0e01b777c554e2df14caacfb7108e09527d648
developer <zhama.hk@gmail.com>
2025-12-19 19:00:14 +0800

    fix: image

--- src/components/FeaturesClient.tsx
+++ src/components/FeaturesClient.tsx
@@ -88,7 +88,7 @@ const features = [
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
       </svg>
     ),
-    image: '/images/features/home-04.png',
+    image: '/images/features/home-03-1.png',
   },
 ];
 
@@ -109,7 +109,7 @@ const moreFeatures = [
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
       </svg>
     ),
-    image: '/images/features/knowledge-assets.jpg',
+    image: '/images/features/knowledge-assets-02.png',
   },
   {
     key: 'financeManagement',
